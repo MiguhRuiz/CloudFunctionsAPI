9062eab3c77acdcc0d1f03d06417a49f41f80c3a
Miguh Ruiz <miguhruiz@gmail.com>
2017-07-10 16:54:23 +0200

    [Add] Initial Files & function to GET route

---
+++ functions/index.js
@@ -0,0 +1,17 @@
+const functions = require('firebase-functions');
+const firebase = require('firebase-admin')
+const config = require('./firebase-config.json')
+
+firebase.initializeApp({
+    credential: firebase.credential.cert(config),
+    databaseURL: 'https://functions-api.firebaseio.com/'
+})
+
+exports.apartaments = functions.https.onRequest((req, res) => {
+    if(req.method == 'GET') {
+        const apartaments = firebase.database().ref('/apartaments')
+        apartaments.on('value', (snapshot) => {
+            res.json(snapshot.val())
+        })
+    }
+})
